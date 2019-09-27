9eed58b0b08c760f0297de56443223983806000b
DB <dbergan@gmail.com>
2019-09-27 15:05:38 -0500

    2019-09-27

--- LOC_Promotions.sql
+++ LOC_Promotions.sql
@@ -59,6 +59,8 @@ INSERT OR REPLACE INTO LocalizedText
 ('BW_LOC_PROMOTION_SURVIVALIST_NAME', 'Survivalist', 'en_US'),
 ('BW_LOC_PROMOTION_SURVIVALIST_DESCRIPTION', 'Heal after any action', 'en_US'),
 
+('BW_LOC_PROMOTION_TERROR_AND_PANIC_NAME', 'Terror and Panic', 'en_US'),
+('BW_LOC_PROMOTION_TERROR_AND_PANIC_DESCRIPTION', 'Pillaging only costs 1 [ICON_Movement] movement', 'en_US'),
 
 
 
@@ -68,23 +70,43 @@ INSERT OR REPLACE INTO LocalizedText
 
 
 
-('BW_LOC_PROMOTION_HEAVY_INFANTRY_PHALANX_NAME', 'Phalanx', 'en_US'),
+('BW_LOC_PROMOTION_HEAVY_INFANTRY_PHALANX_NAME', 'Phalanx Formation', 'en_US'),
 ('BW_LOC_PROMOTION_HEAVY_INFANTRY_PHALANX_DESCRIPTION', '+10 [ICON_STRENGTH] Combat Strength on defense', 'en_US'),
+('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_HEAVY_INFANTRY_PHALANX_CHAPTER_HISTORY_PARA_1', '', 'en_US'),
 
-('BW_LOC_PROMOTION_HEAVY_INFANTRY_SARISSA_NAME', 'Sarissa', 'en_US'),
-('BW_LOC_PROMOTION_HEAVY_INFANTRY_SARISSA_DESCRIPTION', '+5 [ICON_Strength] Combat Strength when attacking or defending land melee units (e.g. Heavy Infantry, Heavy Cavalry)', 'en_US'),
+('BW_LOC_PROMOTION_HEAVY_INFANTRY_OBLIQUE_ORDER_NAME', 'Oblique Order', 'en_US'),
+('BW_LOC_PROMOTION_HEAVY_INFANTRY_OBLIQUE_ORDER_DESCRIPTION', '+5 [ICON_Strength] Combat Strength when attacking or defending against Heavy Infantry', 'en_US'),
+('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_HEAVY_INFANTRY_OBLIQUE_ORDER_CHAPTER_HISTORY_PARA_1', 'The oblique order is a military tactic among heavy infantry battles where an attacking army deploys an asymmetical line formation in an attempt to overwhelm an enemy flank. The commander concentrates the majority of their strength (elite units and/or a greater quantity of troops) on one flank while the remainder fixes the enemy line. This technique allows a commander with weaker or equal total forces to achieve a quick local superiority in numbers on one side and then snowball that advantage down the rest of the line.[NEWLINE][NEWLINE]In the oblique order attack, the line is intentionally thinner on one side in order to concentrate troops on the other side. Then, when meeting the opposing heavy infantry, they would then create an angled (oblique) engagement with the enemy, refusing complete engagement on the weakened flank, but attacking in full on the stronger flank with a concentration of force. Once the strong flank had dominated its oppososition, the commander would wheel the troops 90 degrees to roll up the enemy line.[NEWLINE][NEWLINE]The first recorded use of this sort of tactic was in 371 BCE at the Battle of Leuctra, in Greece, when the Thebans under Epaminondas defeated the Spartans by highly enforcing one flank to fifty rows deep, rather than spreading forces parsimoniously. Philip of Macedon learned Epaminondas'' technique while held prisoner at Thebes, and his descendants (including Alexander), used it in their campaigns. King Frederick II of Prussia also particularly favored this tactic.', 'en_US'),
 
-('BW_LOC_PROMOTION_HEAVY_INFANTRY_TESTUDO_NAME', 'Testudo', 'en_US'),
+
+('BW_LOC_PROMOTION_HEAVY_INFANTRY_TESTUDO_NAME', 'Testudo Formation', 'en_US'),
 ('BW_LOC_PROMOTION_HEAVY_INFANTRY_TESTUDO_DESCRIPTION', '+10 [ICON_Strength] Combat Strength when defending ranged attacks', 'en_US'),
-
+('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_HEAVY_INFANTRY_TESTUDO_CHAPTER_HISTORY_PARA_1', 'In Ancient Roman warfare, the testudo (tortoise) formation was a type of shield wall commonly used by the Roman Legions where the men would align their shields to protect the unit from all incoming projectiles. The first row of men would hold their shields from about the height of their shins to their eyes, so as to cover the formation''s front. The men in the back ranks would place their shields over their heads to protect the formation from above, balancing the shields on their helmets, overlapping them. If necessary, the legionaries on the sides and rear of the formation could stand sideways or backwards with shields held as the front rows, so as to protect the formation''s sides and rear. This reduced the speed and mobility of the formation, but offered excellent protection against arrows and other missile attacks.', 'en_US'),
 
 ('BW_LOC_PROMOTION_LIGHT_INFANTRY_SCREEN_NAME', 'Screen', 'en_US'),
 ('BW_LOC_PROMOTION_LIGHT_INFANTRY_SCREEN_DESCRIPTION', 'When next to a friendly land melee unit: adjacent enemies -7 [ICON_Strength] defense in land melee battles', 'en_US'),
+('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_LIGHT_INFANTRY_SCREEN_CHAPTER_HISTORY_PARA_1', 'Light infantry was deployed as a vanguard, flank guard, or rearguard to screen a tactical position or a larger body of friendly troops from enemy advances. They were usually deployed in a skirmish line, an irregular open formation much more spread out in depth and breadth than a traditional line formation. Their purpose was to harass the enemy, engaging them in only light or sporadic combat in order to delay their movement, disrupt their attack, or weaken their morale. The skirmishers'' open formations and smaller numbers can give them superior mobility over the regular forces, allowing them to fight on more favorable terms, taking advantage of better position or terrain and quickly withdrawing from any threat of superior enemy forces.[NEWLINE][NEWLINE]Though often critical in screening the main army from sudden enemy attacks, skirmish troops couldn''t take or defend ground from heavy infantry or heavy cavalry.', 'en_US'),
 
 ('BW_LOC_PROMOTION_LIGHT_INFANTRY_FLANK_GUARD_NAME', 'Flank Guard', 'en_US'),
 ('BW_LOC_PROMOTION_LIGHT_INFANTRY_FLANK_GUARD_DESCRIPTION', 'When next to a friendly land melee unit: adjacent enemies -7 [ICON_Strength] attack in land melee battles', 'en_US'),
+-- ('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_LIGHT_INFANTRY_FLANK_GUARD_CHAPTER_HISTORY_PARA_1', '', 'en_US'),
 
 
+('BW_LOC_PROMOTION_LIGHT_CAVALRY_ENVELOPE_NAME', 'Envelope', 'en_US'),
+('BW_LOC_PROMOTION_LIGHT_CAVALRY_ENVELOPE_DESCRIPTION', 'Adjacent enemies: -5 [ICON_Strength] defense in land melee battles', 'en_US'),
+('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_LIGHT_CAVALRY_ENVELOPE_CHAPTER_HISTORY_PARA_1', 'Envelopment is the military tactic of seizing objectives in the enemy''s rear with the goal of destroying specific enemy forces and denying them the ability to withdraw. Rather than attacking the enemy head-on, an envelopment seeks to exploit the enemy''s flanks, attacking them from multiple directions and inducing a psychological shock on the defender. An envelopment will consist of the enveloping forces, which attacks the enemy''s flank(s), and a fixing force, which attacks the enemy''s front and "fixes" them in place so that they cannot withdraw or shift their focus on the enveloping forces.', 'en_US'),
+
+('BW_LOC_PROMOTION_LIGHT_CAVALRY_DUST_STORM_NAME', 'Dust Storm', 'en_US'),
+('BW_LOC_PROMOTION_LIGHT_CAVALRY_DUST_STORM_DESCRIPTION', 'Adjacent enemies: -5 [ICON_Strength] when attacking', 'en_US'),
+('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_LIGHT_CAVALRY_DUST_STORM_CHAPTER_HISTORY_PARA_1', 'Another way the Mongols used deception and terror was by tying tree branches or leaves behind their horses. They dragged the foliage behind them in a systematic fashion to create dust storms.', 'en_US'),
+
+('BW_LOC_PROMOTION_LIGHT_CAVALRY_PARTHIAN_SHOT_NAME', 'Parthian Shot', 'en_US'),
+('BW_LOC_PROMOTION_LIGHT_CAVALRY_PARTHIAN_SHOT_DESCRIPTION', '+5 [ICON_STRENGTH] Combat Strength on defense', 'en_US'),
+('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_LIGHT_CAVALRY_PARTHIAN_SHOT_CHAPTER_HISTORY_PARA_1', 'The Parthian shot is a light cavalry military tactic made famous in the West by the Parthians, an ancient Iranian people. While retreating at full gallop, their horse archers would turn their bodies back to shoot at the pursuing enemy from over the horses'' tails. The maneuver required superb equestrian skills, since the rider''s hands were occupied with the bow. As the stirrup had not been invented at the time of the Parthians, the rider relied solely on pressure from his legs to guide his horse.[NEWLINE][NEWLINE]In addition to the Parthians, this tactic was used by most nomads of the Eurasian steppe, including the Scythians, Huns, Turks, Magyars, and Mongols. The technique was used to great effect in Parthia''s victory over the Rome in the Battle of Carrhae where 20,000 Romans were killed (and another 10,000 captured) compared to Parthia''s loss of only a few dozen horses. Marcus Licinius Crassus (of the First Triumvirate) and his son Publius led the Romans in their defeat, and both were killed by the Parthians.', 'en_US'),
+
+('BW_LOC_PROMOTION_LIGHT_CAVALRY_STRING_OF_MARES_NAME', 'String of Mares', 'en_US'),
+('BW_LOC_PROMOTION_LIGHT_CAVALRY_STRING_OF_MARES_DESCRIPTION', '+1 [ICON_Movement] movement and heal after any action', 'en_US'),
+('LOC_PEDIA_UNITPROMOTIONS_PAGE_BW_PROMOTION_LIGHT_CAVALRY_STRING_OF_MARES_CHAPTER_HISTORY_PARA_1', 'Each Mongol soldier typically maintained 3 or 4 horses. Changing horses often allowed them to travel at high speed for days without stopping or wearing out the animals. When one horse tired, the rider would dismount and rotate to another. Though the used mount would still have to travel, it would do so without the weight of the rider. Their ability to live off the land, and in extreme situations off their animals (mare''s milk especially), made their armies far less dependent on the traditional logistical apparatus of agrarian armies. In some cases, as during the invasion of Hungary in early 1241, they covered up to 100 miles (160 km) per day, which was unheard of by other armies of the time. The horse was viewed much like a cow in Mongolia; milked and slaughtered for meat as such. Since most of the Mongols'' mounts were mares, they were able to live off their horses'' milk or milk products as they moved through enemy territory. In dire straits, the Mongol warrior could drink some of the blood from his string of remounts. They could survive a whole month only by drinking mare''s milk combined with mare''s blood.', 'en_US'),
 
 /*
 ('BW_LOC_PROMOTION_AIR_ATTACK__NAME', '', 'en_US'),
